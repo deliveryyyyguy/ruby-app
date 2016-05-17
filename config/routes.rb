1f8d588b6fe4ea7d33bddc03a5c3ae4750a79084
contour <deliveryyyyguy@gmail.com>
2016-05-17 15:25:49 -0400

    sign up users from ui

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   get 'welcome/about', to: 'welcome#about'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  #post 'users', to: 'users#create'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
