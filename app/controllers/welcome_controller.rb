3efd7385ca305f2fe5c95084b73f95990f5eed20
contour <deliveryyyyguy@gmail.com>
2016-05-18 09:10:40 -0400

    Complete Authentication system

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController 
-
+	#home page redirect if niggaa logged in
+	#to the dealspage
   def home
+  	redirect_to articles_path if logged_in?
+  end
+  def about
 
   end
-
 end
