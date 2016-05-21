03aec87148b2e934956338f996826b429ceb3903
contour <deliveryyyyguy@gmail.com>
2016-05-21 10:37:15 -0400

    Fuxxed test and update nav bar for categories and admin make new category

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,7 +4,8 @@ class CategoriesControllerTest < ActionController::TestCase
 
 	def setup
 		@category = Category.create(name: "discount")
-		
+		@user = User.create(username: "john", email: "john@example.com", password:"password", admin: true)
+	
 	end
 
 	test "should get categories index" do
@@ -14,15 +15,21 @@ class CategoriesControllerTest < ActionController::TestCase
 	end
 
 	test "should get new" do
+		session[:user_id] = @user.id
 		get :new
 		assert_response :success
-
-
 	end
 
 	test "should get now" do
 		get(:show, {'id' => @category.id})
 		assert_response :success
+	end
+
+	test "should redirect create when admin not loggin" do
+		assert_no_difference 'Category.count' do
+			post :create, category: { name: "discount" }
+		end
+		assert_redirected_to categories_path
 	end
 
 end
